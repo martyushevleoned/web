5e855f5a48c0c6324988b6f639e0fce9e691d99a
Martyushev Leoned <56407820+martyushevleoned@users.noreply.github.com>
2023-11-12 19:50:17 +0500

    document

--- projects/js_query/js/scripts.js
+++ projects/js_query/js/scripts.js
@@ -1,12 +1,17 @@
 async function query() {
 
-	let url='http://localhost:8080/my-projects/1';
-	let response = await fetch(url);
+	let url = new URL('http://localhost:8080/my-projects');
+	let params = new URLSearchParams({
+		id: document.getElementById('in').value
+	});
+
+
+	let response = await fetch(url + '?' + params);
 
 	if (response.ok) {
 
 	  let json = await response.json();
-	  addElement(json);
+	  addNode(json);
 
 	} else {
 
@@ -15,11 +20,9 @@ async function query() {
 	}
 }
 
-async function addElement(json) {
+function addNode(json) {
 
 	console.log(json);
-	console.log(json.id);
-	console.log(json.username);
 
 	let span = document.createElement("span");
 	span.innerText = json.username;
@@ -31,3 +34,13 @@ async function addElement(json) {
 	let wrapper = document.getElementById('w');
 	wrapper.appendChild(div);
 }
+
+function removeNode(id) {
+
+	document.getElementById(id).remove();
+}
+
+
+function openPage() {
+	window.location = 'https://www.youtube.com/watch?v=wtnkgnERhXE';
+} 
