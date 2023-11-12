d8bab19394c1461f38d167d637c31c3034b29cb6
Martyushev Leoned <56407820+martyushevleoned@users.noreply.github.com>
2023-11-12 13:37:10 +0500

    script

--- projects/js_query/js/scripts.js
+++ projects/js_query/js/scripts.js
@@ -1,5 +1,33 @@
-function add() {
-	const wrapper = document.querySelector('.wrapper');
-	console.log("some");
+async function query() {
 
+	let url='http://localhost:8080/my-projects/1';
+	let response = await fetch(url);
+
+	if (response.ok) {
+
+	  let json = await response.json();
+	  addElement(json);
+
+	} else {
+
+	  alert('Ошибка HTTP: ' + response.status);
+
+	}
+}
+
+async function addElement(json) {
+
+	console.log(json);
+	console.log(json.id);
+	console.log(json.username);
+
+	let span = document.createElement("span");
+	span.innerText = json.username;
+
+	let div = document.createElement("div");
+	div.className = "content";
+	div.appendChild(span);
+
+	let wrapper = document.getElementById('w');
+	wrapper.appendChild(div);
 }
